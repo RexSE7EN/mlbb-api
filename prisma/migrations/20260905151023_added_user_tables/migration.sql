64da2b4f28094df9ea447ed3c41292bca7257025
RexSE7EN <36422471+RexSE7EN@users.noreply.github.com>
2026-09-05 23:06:29 +0545

    feat: Implement user authentication with registration and login functionality

---
+++ prisma/migrations/20260905151023_added_user_tables/migration.sql
@@ -0,0 +1,38 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_api_tokens" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "user_api_tokens_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_api_tokens_token_key" ON "user_api_tokens"("token");
+
+-- AddForeignKey
+ALTER TABLE "user_api_tokens" ADD CONSTRAINT "user_api_tokens_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,13 +1,116 @@
 import { Request, Response } from 'express';
+import { prisma } from '@/config/db.js';
+import bcrypt from 'bcryptjs';
+import generateToken  from '@/utils/generateToken.js';
+import { parseDuration } from '@/utils/dateTimeConverter.js';
 
 const registerUser = async (req: Request, res: Response) => {
-  const body = req.body;
+  const { name, email, password } = req.body;
 
-  res.json({
-    message: 'Register user endpoint',
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Please provide name, email, and password.' });
+  }
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (userExists) {
+    return res.status(400).json({ message: 'User already exists. Please check your email.' });
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(27);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create User
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    }
+  });
+
+  return res.status(201).json({ status: 'success', message: 'User registered successfully', 
+      data: {
+        user : {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: user.createdAt,
+        },
+     }
+   });
+};
+
+const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (!user) {
+    return res.status(401).json({ message: 'Invalid email or password.' });
+  }
+
+  // Verify password
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid email or password.' });
+  }
+
+  // Generate JWT token
+  const token = generateToken(user.id, res);
+  const expiresIn = process.env.JWT_EXPIRATION || '1d';
+
+  const existingToken = await prisma.userAPIToken.findFirst({
+    where: {
+      userId: user.id,
+    },
+  });
+
+  if (!existingToken) {
+    // Log token in db
+    const tokenLog = await prisma.userAPIToken.create({
+      data: {
+        userId: user.id,
+        token: token,
+        expiresAt: new Date(Date.now() + parseDuration(expiresIn)),
+      }
+    });
+  }
+  else {
+    const tokenLog = await prisma.userAPIToken.update({
+      where: {
+        id: existingToken.id,
+      },
+      data: {
+        token: token,
+        expiresAt: new Date(Date.now() + parseDuration(expiresIn)),
+      }
+    });
+  }
+
+  return res.status(200).json({ status: 'success', message: 'User login successful', 
+    data: { 
+      user : {
+        id: user.id, 
+        name: user.name, 
+        email: user.email,
+      },
+      token,
+    } 
   });
 };
 
 export {
   registerUser,
+  loginUser,
 };
